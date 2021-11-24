853b568460d1782e3feef77131882c3519ec5a73
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-24 11:56:58 +0530

    Add Peak and Pop Method

--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -9,6 +9,19 @@ public class Stack {
 		linkedList.addFirst(data);
 	}
 	
+	public void peak() {
+		System.out.println("Peak Element: "+linkedList.peek());
+	}
+	
+	public void delete() {
+		if(linkedList.size() != 0) {
+			linkedList.pop();
+		}else {
+			System.out.println("Stack Is Empty");
+			return;
+		}
+	}
+	
 	public void display() {
 		for (Integer integer : linkedList) {
 			System.out.println(" "+integer);

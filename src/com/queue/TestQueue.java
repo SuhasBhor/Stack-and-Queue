d847a9311bf6a71a019bc6a6743f5480556a7acc
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-24 12:20:38 +0530

    Add Elements In Queue

---
+++ src/com/queue/TestQueue.java
@@ -0,0 +1,12 @@
+package com.queue;
+
+public class TestQueue {
+	public static void main(String[] args) {
+		Queue queue = new Queue();
+		
+		queue.insert(56);
+		queue.insert(30);
+		queue.insert(70);
+		queue.display();
+	}
+}
